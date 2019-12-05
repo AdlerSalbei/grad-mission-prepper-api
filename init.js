46de7b0f537f186da01b880fe28820cfbdba9d2b
Salbei <salbei@gruppe-adler.de>
2019-12-05 16:53:41 +0100

    started work on file changes

--- init.js
+++ init.js
@@ -1,5 +1,6 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs-extra');
+/*
 http.createServer(function (req, res) {
   fs.readFile('description.ext', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -7,3 +8,26 @@ http.createServer(function (req, res) {
     res.end();
   });
 }).listen(8080);
+*/
+
+
+
+function changeDescription (values) {
+  var data = fs.readFileSync("missionFile/co_template.vr/description.ext", 'utf-8');
+
+  values.forEach(function(element){
+    var oldElement = element[0];
+    var newElement = element[1];
+    var index = data.indexOf(oldElement);
+
+    if (index !== -1) {
+      data = data.replace(oldElement, newElement);
+    } else {
+      data = data + newElement;
+    };
+  });
+
+  fs.writeFileSync("missionFile/co_template.vr/description.ext", data, 'utf-8');
+};
+
+changeDescription([["missionName = '';", "missionName = 'testR';"], ["name = '';", "name = 'Salbei';"]]);
