2ce8bb790bf213d84765e5a0cc50dfb42d30c235
Salbei <salbei@gruppe-adler.de>
2019-12-12 15:46:27 +0100

    fixed some bugs and improved some functions

--- init.js
+++ init.js
@@ -10,7 +10,7 @@ http.createServer(function (req, res) {
 }).listen(8080);
 */
 
-function createAndCopy (oldDirectory, newDirectory) {
+function createAndCopyDir (oldDirectory, newDirectory) {
   try {
     fs.ensureDirSync(newDirectory);
 
@@ -24,11 +24,19 @@ function createAndCopy (oldDirectory, newDirectory) {
   };
 };
 
-function readDescription (missionName) {
+function createAndCopyFile (oldFile, newFile) {
+  try {
+    fs.copySync(oldFile, newFile);
+  } catch (err) {
+    console.error(err);
+  };
+};
+
+function readDataFromFile (fileName) {
   let data = "";
 
   try {
-    data = fs.readFileSync(missionName + "/description.ext", 'utf-8');
+    data = fs.readFileSync(fileName, 'utf-8');
   } catch (err) {
     console.error(err);
   };
@@ -36,19 +44,20 @@ function readDescription (missionName) {
   return data
 };
 
-function writeDescription (missionName, newData) {
+function writeDataToFile (missionName, newData) {
   try {
-    fs.writeFileSync(missionName + "/description.ext", newData, 'utf-8');
+    fs.writeFileSync(missionName, newData, 'utf-8');
   } catch (err) {
     console.error(err);
   };
 };
 
-function changeDescription (values, data) {
-  values.forEach(function(element){
+function changeData (changes, data) {
+  changes.forEach(function(element){
     let oldElement = element[0];
     let newElement = element[1];
     let className = element[2];
+    let putBefore = element [3];
     
     switch (element.length) {
       case 1: 
@@ -67,9 +76,19 @@ function changeDescription (values, data) {
         let indexClass = data.indexOf(className);
         let indexBraket = data.indexOf("}", indexClass);
 
-        let firstString = data.substr(0, indexBraket -1);
+        let firstString = data.substr(0, indexBraket);
         let lastString = data.substr(indexBraket, data.length);
+        
+        data = firstString + "\n" + newElement + "\n" + lastString;
 
+        break;
+      case 4: 
+        let indexClass = data.indexOf(className);
+        let indexBraket = data.indexOf(putBefore, indexClass);
+
+        let firstString = data.substr(0, indexBraket);
+        let lastString = data.substr(indexBraket, data.length);
+        
         data = firstString + "\n" + newElement + "\n" + lastString;
 
         break;
@@ -78,47 +97,60 @@ function changeDescription (values, data) {
   return data
 };
 
-function useGradLoadout (missionName, descriptionChanges) {
-  createAndCopy('missionFile/template/modules/grad-loadout', missionName + '/node_modules/grad-loadout');
+function changeFile (fileName, newData) {
+  console.log(newData);
+  let data = readDataFromFile(fileName);
+  data = changeData(newData, data);
+  console.log(data);
+  writeDataToFile(fileName, data);
+};
+
+function useGradLoadout (missionName, descriptionChanges, userChanges) {
+  createAndCopyDir('missionFile/template/modules/grad-loadout', missionName + '/node_modules/grad-loadout');
 
   descriptionChanges.push(
+    [""],
     ["//GRAD LOADOUT ================================================================"],
     ["class Loadouts {"],
-    ["    baseDelay = 10; // minimum time to wait after connect before applying loadout"],
-    ["    perPlayerDelay = 1; // added random delay based on number of players"],
-    ["    handleRadios = 0; // if radios should be handled. defaults to 0"],
-    ["    resetLoadout = 1; // start with empty loadouts instead of modifying existing loadout"],  
+    ["    baseDelay = " + userChanges[0] + "; // minimum time to wait after connect before applying loadout"],
+    ["    perPlayerDelay = " + userChanges[1] + "; // added random delay based on number of players"],
+    ["    handleRadios = " + userChanges[2] + "; // if radios should be handled. defaults to 0"],
+    ["    resetLoadout = " + userChanges[3] + "; // start with empty loadouts instead of modifying existing loadout"],  
     ["};"],
-    ["", "    #include <node_modules\\grad-loadout\\CfgFunctions.hpp>", "CfgFunctions"]
-
+    ["", "    #include <node_modules\\grad-loadout\\CfgFunctions.hpp>", "CfgFunctions"],
+    [""]
   );
 
   return descriptionChanges
 };
 
-function useGradCivs (missionName, descriptionChanges) {
-  createAndCopy('missionFile/template/modules/grad-loadout', missionName + '/node_modules/grad-loadout');
-  createAndCopy('missionFile/template/modules/grad-loadout', missionName + '/node_modules/grad-loadout');
-  createAndCopy('missionFile/template/modules/grad-loadout', missionName + '/node_modules/grad-loadout');
+function useGradCivs (missionName, descriptionChanges, userChanges) {
+  createAndCopyDir('missionFile/template/modules/grad-civs', missionName + '/node_modules/grad-civs');
+  createAndCopyFile('missionFile/template/modules/grad-civs-additionalFiles/fn_initCivs.sqf', missionName + '/functions/missionSetup/fn_initCivs.sqf');
+  createAndCopyFile('missionFile/template/modules/grad-civs-additionalFiles/fn_initCivsEquip.sqf', missionName + '/functions/missionSetup/fn_initCivsEquip.sqf');
+
+  changeFile(missionName + "/functions/missionSetup/cfgFunctions.hpp", [["", "        class initCivs {};", "class intro {};"], ["", "        class initCivsEquip {preInit = 1;};", "class intro {}"]]);
+  
+  changeFile(missionName + "/functions/missionSetup/fn_initMission.sqf", ["", "[] call grad_missionSetup_fnc_initCivs;", "[] call grad_missionSetup_fnc_createDiaryRecords;", "["]);
 
   descriptionChanges.push(   
     ["//GRAD CIVS ================================================================"],
     ["class CfgGradCivs {"],
-    ["    autoInit = 1;"],
-    ["    maxCivsOnFoot = 20;"],
-    ["    maxCivsResidents = 30;"],
-    ["    maxCivsInVehicles = 10;"],  
-    ["    spawnDistancesOnFoot[] = {1000,4500};"],  
-    ["    spawnDistancesInVehicles[] = {1000,4500};"],  
-    ["    debugCivState = 0;"],  
-    ["    debugFps = 0;"],  
-    ["    minCivUpdateTime = 3;"],  
-    ["    minFps = 35;"],  
-    ["    automaticVehicleGroupSize = 1;"],  
-    ["    exitOn = '';"],  
-    ["    onSpawn = '';"],  
-    ["    onHeldUp = ''; "],  
-    ["    backpackProbability = 0.5;"],  
+    ["    autoInit = " + userChanges[0] + ";"],
+    ["    maxCivsOnFoot = " + userChanges[1] + ";"],
+    ["    maxCivsResidents = " + userChanges[2] + ";"],
+    ["    maxCivsInVehicles = " + userChanges[3] + ";"],  
+    ["    spawnDistancesOnFoot[] = " + userChanges[4] + ";"],  
+    ["    spawnDistancesInVehicles[] = " + userChanges[5] + ";"],  
+    ["    debugCivState = " + userChanges[6] + ";"],  
+    ["    debugFps = " + userChanges[7] + ";"],  
+    ["    minCivUpdateTime = " + userChanges[8] + ";"],  
+    ["    minFps = " + userChanges[9] + ";"],  
+    ["    automaticVehicleGroupSize = " + userChanges[10] + ";"],  
+    ["    exitOn = " + userChanges[11] + ";"],  
+    ["    onSpawn = " + userChanges[12] + ";"],  
+    ["    onHeldUp = " + userChanges[13] + ";"],  
+    ["    backpackProbability = " + userChanges[14] + ";"],  
     [""],  
     ["    clothes[] = {"],  
     ["    };"], 
@@ -133,10 +165,9 @@ function useGradCivs (missionName, descriptionChanges) {
     ["    };"],
     [""], 
     ["    backpacks[] = {"], 
-    ["    };"],
-    [""], 
+    ["    };"], 
     ["};"],
-    ["", "    #include <modules\grad-civs\cfgFunctions.hpp>", "CfgFunctions"]
+    ["", "    #include <modules\\grad-civs\\cfgFunctions.hpp>", "CfgFunctions"]
   );
 
   return descriptionChanges
@@ -153,16 +184,18 @@ function removeFolder (missionName) {
 let descriptionChanges = [];
 let missionName = "missionFile/CO_Template.VR";
 let data = "";
+let gradLoadoutChanges = ["10", "1", "0", "1"];
+let gradCivsChanges = ["1", "20", "30", "10", "{1000,4500}", "{1000,4500}", "0", "0", "3", "35", "1", "''", "''", "0.5"];
 
-createAndCopy('missionFile/template/CO_Template.VR', missionName);
+createAndCopyDir('missionFile/template/CO_Template.VR', missionName);
 
-data = readDescription(missionName);
+data = readDataFromFile(missionName + "/description.ext");
 
-descriptionChanges = useGradLoadout(missionName, descriptionChanges);
-descriptionChanges = useGradCivs(missionName, descriptionChanges);
+descriptionChanges = useGradLoadout(missionName, descriptionChanges, gradLoadoutChanges);
+descriptionChanges = useGradCivs(missionName, descriptionChanges, gradCivsChanges);
 
-data = changeDescription(descriptionChanges, data);
+data = changeData(descriptionChanges, data);
 
-writeDescription (missionName, data);
+writeDataToFile(missionName + "/description.ext", data);
 //removeFolder(missionName)
 
